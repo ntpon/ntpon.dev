b66b85eee4b2729874c878f128b34835c937c5fc
art0254 <nana.ort@gmail.com>
2021-06-29 17:04:14 +0700

    Create layout & main-navigations

---
+++ components/layout/logo.js
@@ -0,0 +1,5 @@
+import classes from './logo.module.css';
+function Logo() {
+  return <div className={classes.logo}>art0254</div>;
+}
+export default Logo;
---
+++ components/layout/main-navigation.js
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import Logo from './logo';
+import classes from './main-navigation.module.css';
+function MainNavigation() {
+  return (
+    <header className={classes.header}>
+      <Link href="/">
+        <a>
+          <Logo />
+        </a>
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/posts">Posts</Link>
+          </li>
+          <li>
+            <Link href="/projects">Projects</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+export default MainNavigation;
